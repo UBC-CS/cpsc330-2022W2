45f0e096e92175a28254f8f517d2fd95205e334c
Mathias <mathias@lecuyer.me>
2023-03-26 18:57:29 -0700

    .

--- lectures/code/mglearn_utils.py
+++ lectures/code/mglearn_utils.py
@@ -265,3 +265,33 @@ def plot_two_hidden_layer_graph():
         nn_graph.edge("h2[%d]" % i, "y", label="")
 
     return nn_graph
+
+def plot_cross_validation():
+    plt.figure(figsize=(12, 2))
+    plt.title("cross_validation")
+    axes = plt.gca()
+    axes.set_frame_on(False)
+
+    n_folds = 5
+    n_samples = 25
+
+    n_samples_per_fold = n_samples / float(n_folds)
+
+    for i in range(n_folds):
+        colors = ["w"] * n_folds
+        colors[i] = "grey"
+        bars = plt.barh(
+            y=range(n_folds), width=[n_samples_per_fold - 0.1] * n_folds,
+            left=i * n_samples_per_fold, height=.6, color=colors, hatch="//",
+            edgecolor='k', align='edge')
+    axes.invert_yaxis()
+    axes.set_xlim(0, n_samples + 1)
+    plt.ylabel("CV iterations")
+    plt.xlabel("Data points")
+    plt.xticks(np.arange(n_samples_per_fold / 2., n_samples,
+                         n_samples_per_fold),
+               ["Fold %d" % x for x in range(1, n_folds + 1)])
+    plt.yticks(np.arange(n_folds) + .3,
+               ["Split %d" % x for x in range(1, n_folds + 1)])
+    plt.legend([bars[0], bars[4]], ['Training data', 'Test data'],
+               loc=(1.05, 0.4), frameon=False)
